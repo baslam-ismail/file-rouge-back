faf5ce581b4552f39665453c44de954d6e8b0c16
Papa Abdou Mbodji <abdoumbodji1401@gmail.com>
2025-01-27 11:11:30 +0100

    Initial commit

---
+++ src/main/java/com/malsi/back_malsi/BackMalsiApplication.java
@@ -0,0 +1,13 @@
+package com.malsi.back_malsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackMalsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackMalsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malsi/back_malsi/BackMalsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malsi.back_malsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackMalsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
