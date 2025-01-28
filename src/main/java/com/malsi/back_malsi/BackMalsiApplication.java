cf457d3a5ccd416ceb302df856396e819e80b653
Papa Abdou Mbodji <abdoumbodji1401@gmail.com>
2025-01-28 11:32:54 +0100

    Configurations of properties

--- src/main/java/com/malsi/back_malsi/BackMalsiApplication.java
+++ src/main/java/com/malsi/back_malsi/BackMalsiApplication.java
@@ -1,7 +1,9 @@
 package com.malsi.back_malsi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackMalsiApplication {
@@ -10,4 +12,10 @@ public class BackMalsiApplication {
 		SpringApplication.run(BackMalsiApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start() {
+		return args -> {
+			System.out.println("Hello World");
+		};
+	}
 }
